da86118e25cf081df0c04374a51e17d654ff2ab8
Aishwarya Sukale <78548339+aishwaryasukale23@users.noreply.github.com>
2021-07-30 20:00:57 +0530

    Corrected corrupted characters in quotes

--- game/js/quotes.js
+++ game/js/quotes.js
@@ -1,18 +1,18 @@
-var quotes = ["Sometimes the questions are complicated and the answers are simple. â Dr. Seuss",
-			"You donât have to be a mathematician to have a feel for numbers. â John Forbes Nash",
-			"The only way to learn mathematics is to do mathematics. â Paul R. Halmos",
-			"Millions saw the apple fall, but Newton asked why. â Bernard Baruch",
-			"Wherever there is number, there is beauty. â Proclus",
-			"The essence of mathematics lies in its freedom. â Georg Cantor",
-			"Mathematics is a language. â Josiah Willard Gibbs",
-			"Nature is written in mathematical language. â Galileo Galilei",
-			"Go down deep enough into anything and you will find mathematics. â Dean Schlicter",
-			"It is impossible to be a mathematician without being a poet in soul. â Sofia Kovalevskaya",
-			"Mathematics allows for no hypocrisy and no vagueness. â Stendhal",
-			"âObviousâ is the most dangerous word in mathematics. â Eric Temple Bell",
-			"There should be no such thing as boring mathematics. â Edsger W. Dijkstra",
-			"Mathematics is the music of reason. â James Joseph Sylvester",
-			"Pure mathematics is, in its way, the poetry of logical ideas. â Albert Einstein"
+var quotes = ["Sometimes the questions are complicated and the answers are simple. — Dr. Seuss",
+			"You don’t have to be a mathematician to have a feel for numbers. — John Forbes Nash",
+			"The only way to learn mathematics is to do mathematics. — Paul R. Halmos",
+			"Millions saw the apple fall, but Newton asked why. — Bernard Baruch",
+			"Wherever there is number, there is beauty. — Proclus",
+			"The essence of mathematics lies in its freedom. — Georg Cantor",
+			"Mathematics is a language. — Josiah Willard Gibbs",
+			"Nature is written in mathematical language. — Galileo Galilei",
+			"Go down deep enough into anything and you will find mathematics. — Dean Schlicter",
+			"It is impossible to be a mathematician without being a poet in soul. — Sofia Kovalevskaya",
+			"Mathematics allows for no hypocrisy and no vagueness. — Stendhal",
+			"‘Obvious’ is the most dangerous word in mathematics. — Eric Temple Bell",
+			"There should be no such thing as boring mathematics. — Edsger W. Dijkstra",
+			"Mathematics is the music of reason. — James Joseph Sylvester",
+			"Pure mathematics is, in its way, the poetry of logical ideas. — Albert Einstein"
 			];
 var currentQuote = 0;
 			
